3df4c4dd48dc29db335b988a5f81af59b662d508
Matías Celasco <matiascelasco@gmail.com>
2015-11-22 16:56:38 -0300

    amount of generated objects now is random (40-100)

--- src/tester/MonitorTester.java
+++ src/tester/MonitorTester.java
@@ -4,6 +4,7 @@ import generator.MockGenerator;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -19,7 +20,8 @@ import model.Notification;
 public class MonitorTester {
 	
     public static void main(String[] args) throws IOException {
-		List<Notification> li = MockGenerator.createMockInstances(Notification.class, 40);
+    	int amount = new Random().nextInt(100 - 40) + 40;
+		List<Notification> li = MockGenerator.createMockInstances(Notification.class, amount);
 
 		Gson gson = new GsonBuilder()
 			.registerTypeAdapter(Date.class, new DateSerializer())
