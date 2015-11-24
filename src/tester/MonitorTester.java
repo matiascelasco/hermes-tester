6a8d9b0916c5307ea1fc145ada47cc3b2722594c
Matías Celasco <matiascelasco@gmail.com>
2015-11-24 13:06:31 -0300

    Now response is printed to console. Bug magically solved

--- src/tester/MonitorTester.java
+++ src/tester/MonitorTester.java
@@ -2,11 +2,15 @@ package tester;
 
 import generator.MockGenerator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
@@ -19,34 +23,47 @@ import model.Notification;
 
 public class MonitorTester {
 	
-    public static void main(String[] args) {
-    	try{
-    		String postUrl="http://localhost:8000/load-notifications";
-    		HttpClient client = HttpClientBuilder.create().build();
-    		HttpPost post = new HttpPost(postUrl);
-    		//int amount = new Random().nextInt(100 - 40) + 40;
-    		for(int i=3; i<8; i++){
-    			List<Notification> li = MockGenerator.createMockInstances(Notification.class, i/*amount*/);
+    public static void main(String[] args) throws ClientProtocolException, IOException, InterruptedException {
+		String postUrl = "http://localhost:8000/load-notifications";
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpPost post = new HttpPost(postUrl);
+		Random random = new Random();
+		for(int i = 0; i < 5; i++){
+			
+			System.out.println();
+			
+			// generate notifications
+			int amount = random.nextInt(20 - 5) + 5;  // random between 5 and 20
+			List<Notification> li = MockGenerator.createMockInstances(Notification.class, amount);
+			System.out.format("%d random notifications generated\n", amount);
 
-    			Gson gson = new GsonBuilder()
-    					.registerTypeAdapter(Date.class, new DateSerializer())
-    					.create();
-		
-    			String jsonString = gson.toJson(li);			
-		    		
-    			StringEntity postingString =new StringEntity(jsonString);
-    			post.setEntity(postingString);
-    			System.out.println("Message to sent: "+postingString);
-    			//post.setHeader("Content-type", "application/json");			
-    			HttpResponse response = client.execute(post);
-		
-    			System.out.println(response.toString());
-    			Thread.sleep(5000); 
-    		}
-    	}
-    	catch(Exception e){
-    		System.out.println(e.getMessage());
-    	}
+			// serialize
+			System.out.println("Serializing...");
+			Gson gson = new GsonBuilder()
+				.registerTypeAdapter(Date.class, new DateSerializer())
+				.create();	
+			String jsonString = gson.toJson(li);	
+	    	
+			// sent to server
+			post.setEntity(new StringEntity(jsonString));
+			post.setHeader("Content-type", "application/json");			
+			HttpResponse response = client.execute(post);
+			System.out.println("JSON message sent to monitor at " + postUrl);
+	
+			// read response and print to console
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+				response.getEntity().getContent()
+			));
+			System.out.println("Response from monitor:");
+			String line;
+			while ((line = reader.readLine()) != null) {
+		        System.out.println(line);
+		    }
+			
+			// wait
+			System.out.println("Waiting 5 seconds before doing it again...");
+			Thread.sleep(5000);
+		}
 	}
 
 }
